b31a24bba85a3de093b46763cee7907a10f97fd9
preethaw <preethawarrier@gmail.com>
2021-09-28 00:33:24 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,19 @@
+
+class Box
+  {
+    constructor(){
+      this.x = 200;
+      this.y = 200
+      this.width = 50;
+      this.height = 50;
+    }
+    display(){
+      rectMode(CENTER);
+      rect(this.x,this.y,this.width,this.height);
+    }
+  }
+
+   
+  
+
+  
---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+var box1, box2;
+
+function setup() 
+{
+  createCanvas(400, 400);
+  box1 = new Box();
+  box2 = new Box();
+}
+
+function draw() 
+{
+  background(220);
+  box1.display();
+  box2.display();
+}
+
+
+
+
+
+
+
